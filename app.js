12aeee2da4bdab445b5a28a18e46e486594957a1
code3lue <alan.delez@gmail.com>
2017-04-09 00:11:37 -0400

    finally debugged app, no more planes!

--- app.js
+++ app.js
@@ -1,44 +1,19 @@
-$(document).ready(function() {
-
-  var topics = ["basketball", "football", "soccer", "tennis", "baseball", "hockey"]; 
+$(document).ready(function(e) {
   
+  var topics = ["basketball", "football", "soccer", "tennis", "baseball", "hockey"]; 
   for (var i = 0; i < topics.length; i++) {
-
     var sportBtn = $("<button>");
-    sportBtn.addClass("gif");
+    sportBtn.addClass("btn");
     sportBtn.attr("data-sport", topics[i]);
     sportBtn.text(topics[i]);
     $("#buttons").append(sportBtn);
-
-
-$("#searchBox").on("click", function() {
-
-  $(this).val(" ");
-  
-});
-
-$("#searchSub").on("click", function () {
-
-  var submit = $("#searchBox").val().trim();
-  submit = $("<button>");
-  submit.addClass("gif");
-  submit.attr("data-sport", submit);
-  submit.text(submit);
-  $("#buttons").append(submit);
-  
-})
   };
+  });//doc.ready function
 
-
-
-
-
-  $("button").on("click", function() {
-
-    $("#gifs").empty();
-
-    var sport = $(this).attr("data-sport");
-
+$(document).on("click", ".btn", function(e) {
+  e.preventDefault();
+  $("#gifs").empty();
+  var sport = $(this).attr("data-sport");
      var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
         sport + "&?rating=pg13&api_key=dc6zaTOxFJmzC&limit=10"; //limit of 10 gifs
       $.ajax({
@@ -48,8 +23,7 @@ $("#searchSub").on("click", function () {
         .done(function(response) {
           var results = response.data;
           for (var i = 0; i < results.length; i++) {
-
-            //assigning json item into a variable
+//assigning json item into a variable
             var gifDiv = $("<div class='item'>");
             var rating = results[i].rating;
             var p = $("<p>").text("Rating: " + rating);
@@ -65,49 +39,30 @@ $("#searchSub").on("click", function () {
             //<img src=''> p
             gifDiv.prepend(sportImage);
             $("#gifs").append(gifDiv);
-
           }
         }); //response function
-
- 
   });// button click function
-
-
-});//doc.ready function
+  $("#searchBox").on("click", function() {
+  $(this).val(" "); 
+});
+$("#searchSub").on("click", function (e) {
+  e.preventDefault();
+  var text = $("#searchBox").val().trim();
+  submit = $("<button>");
+  submit.addClass("btn");
+  submit.attr("data-sport", text);
+  submit.text(text);
+  $("#buttons").append(submit); 
+});
 
   $(document).on("click", ".gif", function() {
-
-    
-
-  var state = $(this).attr("data-state");
-
-     if (state === "still") {
-
-        $(this).attr("src", $(this).attr("data-animate"));
-        $(this).attr("data-state", "animate");
-
+    var state = $(this).attr("data-state");
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+$(this).attr("data-state", "animate");
       }
-
       else if (state !== "still") {
         $(this).attr('src', $(this).attr('data-still'));
         $(this).attr('data-state', 'still');
-      }
-
-      
+      }  
   }); //doc.gif click function
-
-
-
-
-
-  
-
-
-
-    
-   
-      
-
-
-
-    
